2ded601ba42034ded1ffa76ba492e8bb265b3498
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-11 22:18:11 +0530

    30553

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Restaurant
+from .models import DatabaseError
+from .models import MenuItem
 def home(request):
     try:
         restaurant= Restaurant.objects.get(pk=1)
@@ -12,6 +14,20 @@ def home(request):
                     'restaurant_name': 'My Awesome Restaurant',
                     'restaurant_phone': 'N/A',
                 }
+def menu(request):
+    try:
+        menu_items= MenuItem.objects.all()
+        context={
+            'menu_items': menu_items,
+            'message': None,
+        }
+    except DatabaseError:
+        menu_items=[]
+        context={
+            'menu_items': menu_items,
+            'message': 'WE ARE CURRENTLY EXPERIENCING TECHNICAL DIFFICULTIES. PLEASE STANDBY.'
+        }
+        return render(request,'restaurant_management/menu.html',context)
     return render(request,'restaurant_management/home.html',context)
     def about(request):
         return render(request,'restaurant_management/about.html')
