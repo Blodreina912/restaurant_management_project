d4b7090a7518c357e62d46e57ab0e5f26c9efbbd
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-11 12:53:55 +0530

    29725

--- restaurant_management/context_processor.py
+++ restaurant_management/context_processor.py
@@ -1,3 +1,6 @@
 from django.conf import settings
 def restaurant_name(request):
-    return{'restaurant_name': settings.RESTAURANT_NAME}
+    return{
+        'restaurant_name': getattr(settings,'RESTAURANT_NAME','Restaurant'},
+        'restaurant_phone': getattr(settings,'RESTAURANT_PHONE', 'N/A'),
+        }
