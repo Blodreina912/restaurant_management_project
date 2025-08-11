5a22b53431586a69b6e6cad81008fa1b298d469d
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-11 22:12:15 +0530

    30553

--- restaurant_management/template/restaurant_management/models.py
+++ restaurant_management/template/restaurant_management/models.py
@@ -4,3 +4,9 @@ class Restaurant(models.Model):
     phone_name= models.CharField(max_length=20)
     def__str__(self):
         return self.name
+        class MenuItem(models.Model):
+            name= models.CharField(max_length=100)
+            description= models.TextField()
+            price = models.DecimalField(max_digits=6,decimal_places=2)
+            def__str__(self):
+                return self.name
