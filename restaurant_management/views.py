0756a2618c0c5a02964b00f2267bb2ab8f7707f8
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-11 13:13:38 +0530

    29725

--- restaurant_management/views.py
+++ restaurant_management/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from .models import Restaurant
 def home(request):
+    try:
+        restaurant= Restaurant.objects.get(pk=1)
+        context ={
+            'restaurant_name': restaurant.name,
+            'restaurant_phone': restaurant.phone_number,
+            }
+            except Restaurant.DoesNotExist:
+                context={
+                    'restaurant_name': 'Default Restaurant Name',
+                    'restaurant_phone': 'N/A',
+                }
     return render(request,'restaurant_management/home.html',context)
     def about(request):
         return render(request,'restaurant_management/about.html')
