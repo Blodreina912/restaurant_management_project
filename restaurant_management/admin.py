aa524359ac6aabd93f8b1f5e657ca2f3d7f9d938
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-12 23:00:43 +0530

    31133

--- restaurant_management/admin.py
+++ restaurant_management/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 from .models import UserProfile
 admin.site.register(UserProfile)
+from .models import MenuItem,Order
+admin.site.register(Menu.Item)
+admin.site.register(Order)
