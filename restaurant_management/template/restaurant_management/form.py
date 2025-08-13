2c578da5191bf2b2eba0e9a6fe562acbcad08385
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-13 23:30:34 +0530

    31966

--- restaurant_management/template/restaurant_management/form.py
+++ restaurant_management/template/restaurant_management/form.py
@@ -11,3 +11,7 @@ class FeedbackForm(forms.ModelForm):
         class ContactForm(forms.ModelForm):
             class Meta:
                 model= ContactFormfields=['name','email']
+                widgets= {
+                    'name': form.TextInput(attrs={'placeholder': 'Your Name'}),
+                    'email': forms.EmailInput(attrs={'placeholder': 'Your Email Address'}),
+                }
