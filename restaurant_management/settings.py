368a301642a30c86d3c7eb5163cd3799a4fcafe3
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-13 14:39:51 +0530

    31500

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -127,6 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-RESTAURANT_NAME="Your restaurant name here"
+RESTAURANT_NAME="My Awesome Restaurant"
 RESTAURANT_PHONE="+91 8694853215"
 RESTAURANT_HOURS= "Mon-Fri: 11am-9pm,Sat-Sun: 10am-10pm"6y
+
