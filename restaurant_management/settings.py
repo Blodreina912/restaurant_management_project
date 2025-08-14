d3ea714112ba5f02c249f78ec321de7bab8ec2f0
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-14 23:29:58 +0530

    32404

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -134,3 +134,4 @@ import os
 BASE_DIR= os.path.dirname(os.path.abspath(___file__)))
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+EMAIL_BACKEND= 'django.core.mail.backends.console.EmailBackend'
