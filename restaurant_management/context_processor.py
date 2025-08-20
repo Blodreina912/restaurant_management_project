c449e32b4792cc91c33bd652075fb732fd98c5d4
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-08-20 21:32:24 +0530

    33402

--- restaurant_management/context_processor.py
+++ restaurant_management/context_processor.py
@@ -5,3 +5,7 @@ def restaurant_name(request):
         'restaurant_phone': getattr(settings,'RESTAURANT_PHONE', 'N/A'),
         'restaurant_hours': getattr(settings,'RESTAURANT_HOURS', 'N/A'),
         }
+def cart_total(request):
+    cart= request.session.get('cart',{})
+    total_items= sum(item.get('quantity',0)for item in cart.values())
+    return {'cart_total_items': total_items}
