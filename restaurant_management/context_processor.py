3c0df9311328cea9d2a92401644b6573f26b90c9
Blodreina912 <122105957+Blodreina912@users.noreply.github.com>
2025-09-21 14:04:24 +0530

    33402

--- restaurant_management/context_processor.py
+++ restaurant_management/context_processor.py
@@ -6,6 +6,12 @@ def restaurant_name(request):
         'restaurant_hours': getattr(settings,'RESTAURANT_HOURS', 'N/A'),
         }
 def cart_total(request):
+    total_items = 0
+    if request.user.is_authenticated:
+        total_items= CartItem.objects.filter(user=request.user).aggregate(total=Sum('quantity')['total'] or 0)
+        else:
+            cart=request.session.get('cart',{})
+            total_items= sum(cart.values()) if cart else 0
     cart= request.session.get('cart',{})
     total_items= sum(item.get('quantity',0)for item in cart.values())
     return {'cart_total_items': total_items}
